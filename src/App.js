0a084febd320dd546ae61edb704f9adb7ce67627
Javed <javeedjokhio48@gmail.com>
2023-08-11 02:29:06 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width='400px' sx={{width:{xl:'1488px'}}} m='auto'>
+      <Router> {/* Wrap your routes in a Router component */}
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/exercise/:id' element={<ExerciseDetail />} />
+          
+        </Routes>
+      </Router>
+      <Footer/>
+    </Box>
   );
 }
 
